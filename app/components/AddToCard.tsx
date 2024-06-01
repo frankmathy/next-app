dca019685db37e5cfae57d52ff965b7c762ce146
Frank Mathy <frankmathy@gmail.com>
2024-06-02 00:32:14 +0200

    Added Tailwind and Daisy

--- app/components/AddToCard.tsx
+++ app/components/AddToCard.tsx
@@ -4,7 +4,9 @@ import React from "react";
 function AddToCard() {
   return (
     <div>
-      <button onClick={() => console.log("Click")}>Add to Cart</button>
+      <button className="btn btn-primary" onClick={() => console.log("Click")}>
+        Add to Cart
+      </button>
     </div>
   );
 }
